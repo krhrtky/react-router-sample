c95739dbb5a191b47c7bdc6fe26e6010bced6a0f
Takuya Kurihara <takuya.kurihara00@gmail.com>
2020-06-15 14:26:02 +0900

    Replace redirect process.

--- src/pages/authorized/Authorized.tsx
+++ src/pages/authorized/Authorized.tsx
@@ -1,3 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { push } from 'connected-react-router';
+import type { Store } from '@/store';
 
-export const Authorized = () => <h1>Authorized </h1>;
+export const Authorized = () => {
+  const user = useSelector((store: Store) => store.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!user.authorized) {
+      dispatch(push('/'));
+      return;
+    }
+    console.log('authorized!');
+  }, [user]);
+
+  return <h1>Authorized </h1>;
+};
